5336b58cad42b31097bc521d763df2fb74a8caa9
Jay <jay.sanjay.karia@gmail.com>
2023-04-28 13:20:44 +0530

    use method added

--- src/GameClasses/Potion.java
+++ src/GameClasses/Potion.java
@@ -5,20 +5,30 @@ public class Potion {
     public int duration;
     public String effect;
     public int value;
+    public String key;
 
-    public Potion(String name, int duration, String effect, int value) {
+    public Potion(String name, int duration, String effect, int value, String key) {
         this.name = name;
         this.duration = duration;
         this.effect = effect;
         this.value = value;
-    }
-
-    public void drink() {
-
+        this.key = key;
     }
 
     public void getInfo() {
+        System.out.println(effect);
+    }
 
+    public double use(double damage) {
+        if (key.equals("d")) {
+            damage = (150*damage) / 100;
+            return damage;
+        } else if (key.equals("s")) {
+            damage = (50*damage) / 100;
+            return damage;
+        }
+        else 
+            return 0;
     }
 
 }
